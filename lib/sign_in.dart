c3cb32cc24636931eba4b52b73585a8e2c873612
Muhammad Amin Haiqal <73211654+aminhaiqal@users.noreply.github.com>
2023-05-14 09:57:40 +0800

    add iconsax dependency
    iconsax is icons library for this development

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:iconsax/iconsax.dart';
 
 class SignIn extends StatelessWidget {
   const SignIn({Key? key}) : super(key: key);
