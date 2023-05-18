f439e23eb85ec80553323fb376e9c11e08075d03
Muhammad Amin Haiqal <73211654+aminhaiqal@users.noreply.github.com>
2023-05-18 15:15:28 +0800

    update main.dart
    add firebase package

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:dormchef/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:dormchef/splash_screen.dart';
 import 'package:dormchef/sign_in.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
