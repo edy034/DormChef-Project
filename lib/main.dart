36730f6caeab97afd0fa254511d8f7ee4310dffd
yusriyusoff <127831278+Yusri-Yusoff@users.noreply.github.com>
2023-05-20 06:02:04 +0800

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:dormchef/splash_screen.dart';
 import 'package:dormchef/sign_in.dart';
+import 'package:dormchef/subForm.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -24,3 +32,25 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+/*
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Recipe',
+      //home :Subscription(),
+      home:SubForm(),
+     // home:UserProfile(),
+     //home:IssueRep(),
+    );
+  }
+}*/
