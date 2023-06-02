5b81db4280eddb71ffd56aa9c91f92bef94b6d09
Muhammad Amin Haiqal <73211654+aminhaiqal@users.noreply.github.com>
2023-06-03 05:26:10 +0800

    create 4 new files
    1. screens
    2. controllers
    3. models
    4. database

--- lib/screens/user_profile/profile_overview.dart
+++ lib/screens/user_profile/profile_overview.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:dormchef/text_style.dart';
+import 'package:dormchef/screens/text_style.dart';
 
 class ProfileOverview extends StatefulWidget {
   const ProfileOverview({Key? key}) : super(key: key);
