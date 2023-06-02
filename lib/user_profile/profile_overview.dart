ddfc3d2ab13e7443c664ba670604e33a135c7c87
Muhammad Amin Haiqal <73211654+aminhaiqal@users.noreply.github.com>
2023-06-02 12:03:27 +0800

    Update profile_overview.dart

---
+++ lib/user_profile/profile_overview.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:dormchef/new_style.dart';
+
+class ProfileOverview extends StatefulWidget {
+  const ProfileOverview({Key? key}) : super(key: key);
+
+  static const String routeName = '/profile_overview';
+
+  @override
+  State<ProfileOverview> createState() => _ProfileOverviewState();
+
+}
