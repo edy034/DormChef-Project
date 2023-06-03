1c6e5afd576cfaf287526c1b1b1ee6c7abe6464d
Muhammad Amin Haiqal <73211654+aminhaiqal@users.noreply.github.com>
2023-06-03 16:30:57 +0800

    implement firebase api
    1. Firebase Authentication
    2. Firestore Database

---
+++ lib/models/users.dart
@@ -0,0 +1,72 @@
+class Users {
+  // Properties
+  String uid = '';
+  String username = '';
+  String email = '';
+  String password = '';
+  String bio = '';
+  String phone = '';
+  String subscription = '';
+
+  // Constructor
+  Users();
+
+  // Setters
+  void setUid(String uid) {
+    this.uid = uid;
+  }
+  void setUsername(String username) {
+    this.username = username;
+  }
+  void setEmail(String email) {
+    this.email = email;
+  }
+  void setPassword(String password) {
+    this.password = password;
+  }
+  void setBio(String bio) {
+    this.bio = bio;
+  }
+  void setPhone(String phone) {
+    this.phone = phone;
+  }
+  void setSubscription(String subscription) {
+    this.subscription = subscription;
+  }
+
+  // Getters
+  String getUid() {
+    return uid;
+  }
+  String getUsername() {
+    return username;
+  }
+  String getEmail() {
+    return email;
+  }
+  String getPassword() {
+    return password;
+  }
+  String getBio() {
+    return bio;
+  }
+  String getPhone() {
+    return phone;
+  }
+  String getSubscription() {
+    return subscription;
+  } 
+
+  // Convert user object to map
+  Map<String, dynamic> toMap() {
+    return {
+      'uid': uid,
+      'email': email,
+      'password': password,
+      'username': username,
+      'bio': bio,
+      'phone': phone,
+      'subscription': subscription,
+    };
+  }
+}
