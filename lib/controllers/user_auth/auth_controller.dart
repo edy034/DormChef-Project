faef96ff561b9d969115c7dbf6cf0a5362ee0e80
Muhammad Amin Haiqal <73211654+aminhaiqal@users.noreply.github.com>
2023-06-13 15:46:49 +0800

    done with authentication

--- lib/controllers/user_auth/auth_controller.dart
+++ lib/controllers/user_auth/auth_controller.dart
@@ -3,6 +3,7 @@ import 'package:dormchef/models/users.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 import '../../screens/user_homepage/navigation.dart';
 
